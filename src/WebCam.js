ce3b12316c84746b2ec62e0e8282b99bdb85f13d
SpecOps7 <ctithservices@gmail.com>
2020-11-01 12:25:12 -0600

    Initial commit

---
+++ src/WebCam.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import update from 'immutability-helper';
+
+function WebCam() {
+    //value pair state
+
+    return (
+        <div className="tensorflow">
+            
+        </div>
+    )
+}
+
+export default WebCam
